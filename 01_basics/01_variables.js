6b5f37179caf5bf4ad57477353f5b9bff02e587c
Sunil Kumar Ketha <sunilkumarketha@gmail.com>
2023-10-23 20:26:04 +0000

    Introduction to variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+// Introduction to variables
+const accountId=35540144
+var accountUsername="suketha@gmail.com";
+let accountPassword="Javascript";
+accountCity="Jaipur";
+
+// we should not be using var as it has concerns with the block scope and the functional scope.
+
+// we need to only use  let keyword for variables and assume that there is no keyword var as such
+
+console.log(accountId);
+console.table([accountId,accountUsername,accountPassword,accountCity]);
